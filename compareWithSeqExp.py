cc85181265863a14a20a669abde48204b6708e84
targetakhil <targetakhil@gmail.com>
2021-09-09 21:53:11 +0530

    added scan compare exp

--- compareWithSeqExp.py
+++ compareWithSeqExp.py
@@ -20,16 +20,18 @@ def readCsv():
     parRelative = []
     oldRelative = []
 
-    with open('C:\\Users\\targe\\Documents\\scan_benchmarks\\copyIfCompare.csv') as csvfile:
+    with open('C:\\Users\\targe\\Documents\\scan_benchmarks\\csv_files\\transformInclusiveScanCompare.csv') as csvfile:
         valueReader = csv.reader(csvfile)
         first = True
+        c = 5
         for row in valueReader:
             N = float(row[0])
             seq = float(row[1])
             par = float(row[2])
             old = float(row[3])
 
-            inputSizes.append(N)
+            inputSizes.append(c)
+            c += 1
             seqTime.append(seq)
             parTime.append(par)
             oldTime.append(old)
@@ -37,12 +39,16 @@ def readCsv():
             parRelative.append(seq/par)
             oldRelative.append(seq/old)
     
+    ax.set_xticks(range(5, 32, 1))
+
     ax.plot(inputSizes, parRelative, color=labelToCol['par'])
     ax.plot(inputSizes, oldRelative, color=labelToCol['old'])
+    ax.plot([5, 32], [1, 1], color=labelToCol['seq'])
 
     handles = []
-    handles.append(mpatches.Patch(color=labelToCol['par'], label="Par relative to Seq"))
-    handles.append(mpatches.Patch(color=labelToCol['old'], label="Old relative to Seq"))
+    handles.append(mpatches.Patch(color=labelToCol['seq'], label="seq"))
+    handles.append(mpatches.Patch(color=labelToCol['par'], label="Par"))
+    handles.append(mpatches.Patch(color=labelToCol['old'], label="Old"))
 
     plt.legend(loc="upper right", handles=handles)
     plt.show()
